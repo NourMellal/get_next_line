6af7de4bbeaa3679da8e7964eed8751577093479
NourMellal <mellal.nour9@gmail.com>
2023-12-13 23:17:21 +0100

    i think alll done

--- get_next_line.h
+++ get_next_line.h
@@ -3,16 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmellal <nmellal@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nmellal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 14:54:14 by nmellal           #+#    #+#             */
-/*   Updated: 2023/12/08 14:58:35 by nmellal          ###   ########.fr       */
+/*   Updated: 2023/12/13 23:15:12 by nmellal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <unistd.h>
+# define BUFFER_SIZE 5
 
+size_t	ft_strlen(char *str);
+char	*destalloc(char *str);
+char	*ft_strcat(char *dest, char *src);
+int		charsearch(char *buffer, char c);
+char	*return_and_free(size_t len, char *buffer, char *str);
+char	*readline(int fd, char *buffer, ssize_t len);
+char	*process_line(char *buffer);
+char	*go_to_next_line(char *buffer);
+char	*get_next_line(int fd);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,74 @@
 /*   By: nmellal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 14:01:35 by nmellal           #+#    #+#             */
-/*   Updated: 2023/12/11 21:42:19 by nmellal          ###   ########.fr       */
+/*   Updated: 2023/12/13 23:15:33 by nmellal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	int	i;
+
+	i = -1;
+	if (!str)
+		return (0);
+	while (str[++i])
+		;
+	return (i);
+}
+
+char	*destalloc(char *str)
+{
+	if (!str)
+	{
+		str = malloc(1);
+		if (!str)
+			return (NULL);
+		str[0] = '\0';
+	}
+	return (str);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	dest = destalloc(dest);
+	if (!dest || !src)
+		return (NULL);
+	str = malloc(ft_strlen(dest) + ft_strlen(src) + 1);
+	if (!str)
+		return (NULL);
+	i = -1;
+	while (dest[++i])
+		str[i] = dest[i];
+	j = 0;
+	while (src[j])
+	{
+		str[i + j] = src[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	free(dest);
+	return (str);
+}
+
+int	charsearch(char *buffer, char c)
+{
+	int	i;
+
+	if (!buffer)
+		return (-1);
+	i = 0;
+	while (buffer[i])
+	{
+		if (buffer[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
